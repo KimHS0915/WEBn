394682375bc901a1328a8b0a5a609b4e78881346
KimHS0915 <remember2552@gmail.com>
2020-06-14 23:59:30 +0900

    React Hooks Life Cycle

--- React/class-function/src/FuncComp.jsx
+++ React/class-function/src/FuncComp.jsx
@@ -1,9 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+const funcStyle = 'color:blue';
+let funcId = 0;
 
 function FuncComp(props) {
     const [number, setNumber] = useState(props.initNumber);
     const [time, setTime] = useState((new Date()).toLocaleTimeString());
-  
+
+    useEffect(() => {
+      console.log('%cfunc => useEffect (componentDidMount) ' + (++funcId), funcStyle);
+      return () => {
+        console.log('%cfunc => useEffect return (componentWillUnMount) ' + (++funcId), funcStyle); 
+      }
+    }, []);
+
+    useEffect(() => {
+      console.log('%cfunc => useEffect number (componentDidMount & componentDidUpdate) ' + (++funcId), funcStyle);
+      document.title = number;
+      return () => {
+        console.log('%cfunc => useEffect number return (componentDidMount & componentDidUpdate) ' + (++funcId), funcStyle); 
+      }
+    }, [number]);
+
+    useEffect(() => {
+      console.log('%cfunc => useEffect time (componentDidMount & componentDidUpdate) ' + (++funcId), funcStyle);
+      document.title = time;
+      return () => {
+        console.log('%cfunc => useEffect time return (componentDidMount & componentDidUpdate) ' + (++funcId), funcStyle); 
+      }
+    }, [time]);
+
+ 
+    console.log('%cfunc => render ' + (++funcId), funcStyle);
+
     return (
       <div className="container">
         <h2>function style component</h2>
