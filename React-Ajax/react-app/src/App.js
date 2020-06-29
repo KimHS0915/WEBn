21c1dc7200e8096febe3b29fd19661f198b14313
KimHS0915 <remember2552@gmail.com>
2020-06-29 22:48:34 +0900

    Function Component

--- React-Ajax/react-app/src/App.js
+++ React-Ajax/react-app/src/App.js
@@ -1,48 +1,64 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Nav from './NavClass';
 // import Nav from './NavFunc'; 
 import Article from './ArticleClass';
 
-class App extends Component {
-  state = {
-    article: {
-      title: 'Welcome', 
-      desc: 'Hello, React & Ajax',
-    }
-  }
+// class App extends Component {
+//   state = {
+//     article: {
+//       title: 'Welcome', 
+//       desc: 'Hello, React & Ajax',
+//     }
+//   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>WEB</h1>
-        <Nav onClick={(id) => {
-          fetch(`${id}.json`)
-            .then((result) => {
-              return result.json();
-            })
-            .then((json) => {
-              this.setState({
-                article: {
-                  title: json.title,
-                  desc: json.desc,
-                }
-              });
-            });
-        }}></Nav>
-        <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
-      </div>
-    );
-  }
-}
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <h1>WEB</h1>
-//       <Nav></Nav>
-//       <Article title={} desc={}></Article>
-//     </div>
-//   );
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>WEB</h1>
+//         <Nav onClick={(id) => {
+//           fetch(`${id}.json`)
+//             .then((result) => {
+//               return result.json();
+//             })
+//             .then((json) => {
+//               this.setState({
+//                 article: {
+//                   title: json.title,
+//                   desc: json.desc,
+//                 }
+//               });
+//             });
+//         }}></Nav>
+//         <Article title={this.state.article.title} desc={this.state.article.desc}></Article>
+//       </div>
+//     );
+//   }
 // }
+
+const App = () => {
+  const [article, setArticle] = useState({
+    title: 'Welcome', 
+    desc: 'Hello, React & Ajax',
+  });
+
+  return (
+    <div className="App">
+    <h1>WEB</h1>
+    <Nav onClick={(id) => {
+      fetch(`${id}.json`)
+        .then((result) => {
+          return result.json();
+        })
+        .then((json) => {
+          setArticle({
+            title: json.title,
+            desc: json.desc,
+          });
+        });
+    }}></Nav>
+    <Article title={article.title} desc={article.desc}></Article>
+  </div>
+  );
+}
 
 export default App;
