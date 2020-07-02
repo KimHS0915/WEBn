c7c956401973f8a4b7404b37d3029824f0bd8126
KimHS0915 <remember2552@gmail.com>
2020-07-02 10:43:02 +0900

    Add NowLoading Function Component

--- React-Ajax/react-app/src/AppFunc.jsx
+++ React-Ajax/react-app/src/AppFunc.jsx
@@ -1,42 +1,77 @@
 import React, { useState, useEffect } from 'react';
 import Nav from './NavFunc'; 
 import Article from './ArticleFunc';
+import NowLoading from './NowLoadingFunc';
 
 const App = () => {
   const [article, setArticle] = useState({
-    title: 'Welcome', 
-    desc: 'Hello, React & Ajax',
+    item: {
+      title: 'Welcome', 
+      desc: 'Hello, React & Ajax',
+    },
+    isLoading: false,
   });
-  const [list, setList] = useState([]);
+  const [list, setList] = useState({
+    items: [],
+    isLoading: false,
+  });
 
   useEffect(() => {
+    const newList = Object.assign({}, list, {isLoading: true});
+    setList(newList);
     fetch('list.json')
       .then((result) => {
         return result.json();
       })
       .then((json) => {
-        setList(json);
+        setList({
+          items: json,
+          isLoading: false,
+        });
       });    
   }, []);
 
-  return (
-    <div className="App">
-    <h1>WEB</h1>
-    <Nav list={list} 
+  let NavTag;
+  let ArticleTag;
+
+  if(list.isLoading) {
+    NavTag = <NowLoading />
+  } else {
+    NavTag = <Nav list={list.items} 
     onClick={(id) => {
+      const newArticle = Object.assign({}, article, {isLoading: true});
+      setArticle(newArticle);
       fetch(`${id}.json`)
         .then((result) => {
           return result.json();
         })
         .then((json) => {
           setArticle({
-            title: json.title,
-            desc: json.desc,
+            item: {
+              title: json.title,
+              desc: json.desc,
+            },
+            isLoading: false,
           });
         });
     }}></Nav>
-    <Article title={article.title} desc={article.desc}></Article>
-  </div>
+  }
+
+  if(article.isLoading) {
+    ArticleTag = <NowLoading />
+  } else {
+    ArticleTag = <
+      Article title={article.item.title} 
+      desc={article.item.desc}
+    ></Article>
+  }
+
+  return (
+    <div className="App">
+      <h1>WEB</h1>
+      {NavTag}
+      {ArticleTag}
+    </div>
   );
 }
 
---
+++ React-Ajax/react-app/src/NowLoadingFunc.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NowLoading = () => {
+  return (
+    <div>Now Loading...</div>
+  );
+}
+
+export default NowLoading;
