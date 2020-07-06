97c20e877ff932fd1982267adcb1e85048533112
KimHS0915 <remember2552@gmail.com>
2020-07-07 00:05:00 +0900

    Add DisplayNumberContainer

--- React-Redux/react-app/src/components/DisplayNumberRoot.jsx
+++ React-Redux/react-app/src/components/DisplayNumberRoot.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import DisplayNumber from "./DisplayNumber";
+import DisplayNumber from "../containers/DisplayNumberContainer";
 
 export default class DisplayNumberRoot extends Component {
   render() {
