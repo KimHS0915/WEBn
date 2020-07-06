14498f1a46ca7fe46899cb4b094c64e5435766c0
KimHS0915 <remember2552@gmail.com>
2020-07-06 23:53:14 +0900

    Add AddNumberContainer

--- React-Redux/react-app/src/components/AddNumber.jsx
+++ React-Redux/react-app/src/components/AddNumber.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import store from '../store';
 
 export default class AddNumber extends Component {
   state = {
@@ -11,10 +10,7 @@ export default class AddNumber extends Component {
       <div>
         <h1>Add Number</h1>
         <input type="button" value="+" onClick={() => {
-          store.dispatch({
-            type: 'INCREMENT', 
-            size: this.state.size,
-          });
+          this.props.onClick(this.state.size);
         }}></input>
         <input type="text" value={this.state.size} onChange={(e) => {
           this.setState({
