96c771ce8d21857f710e42a5b372adbace7b9293
KimHS0915 <remember2552@gmail.com>
2020-07-08 23:44:59 +0900

    react-redux connect and provider

--- React-Redux/react-app/src/containers/AddNumberContainer.jsx
+++ React-Redux/react-app/src/containers/AddNumberContainer.jsx
@@ -1,7 +1,12 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import AddNumber from '../components/AddNumber';
-import store from '../store';
+// import store from '../store';
+import { connect } from 'react-redux';
 
+export default connect()(AddNumber);
+
+
+/*
 export default class extends Component {
   render() {
     return <AddNumber onClick={(size) => {
@@ -12,3 +17,4 @@ export default class extends Component {
     }}></AddNumber>
   }
 }
+*/
--- React-Redux/react-app/src/containers/DisplayNumberContainer.jsx
+++ React-Redux/react-app/src/containers/DisplayNumberContainer.jsx
@@ -1,7 +1,11 @@
-import React, { Component } from 'react';
-import store from '../store';
+// import React, { Component } from 'react';
+// import store from '../store';
 import DisplayNumber from '../components/DisplayNumber';
+import { connect } from 'react-redux';
 
+export default connect()(DisplayNumber);
+
+/*
 export default class extends Component {
   state = {number: store.getState().number}
   constructor(props) {
@@ -15,3 +19,4 @@ export default class extends Component {
     return <DisplayNumber number={this.state.number}></DisplayNumber>
   }
 }
+*/
