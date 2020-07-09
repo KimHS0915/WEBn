ecf71ed1c970fc084e5de0f1d66c2707267f8114
KimHS0915 <remember2552@gmail.com>
2020-07-09 23:06:04 +0900

    React Redux - mapDispatchToProps

--- React-Redux/react-app/src/containers/AddNumberContainer.jsx
+++ React-Redux/react-app/src/containers/AddNumberContainer.jsx
@@ -3,8 +3,15 @@ import AddNumber from '../components/AddNumber';
 // import store from '../store';
 import { connect } from 'react-redux';
 
-export default connect()(AddNumber);
+const mapReduxDispatchToReactProps = (dispatch) => {
+  return {
+    onClick: (size) => {
+      dispatch({type: 'INCREMENT', size: size});
+    }
+  }
+}
 
+export default connect(null, mapReduxDispatchToReactProps)(AddNumber);
 
 /*
 export default class extends Component {
